ffe5d544c580605e160810d3f4a46976379d7f61
Sanzhar <58833843+SanXzhar@users.noreply.github.com>
2022-07-13 10:21:23 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import ReactDOM from "react-dom/client";
+import {BrowserRouter, Routes, Route, } from "react-router-dom";
+
 import './App.css'
+import StartPage from "./pages/StartPage/StartPage";
+
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <StartPage />
+  {/* <BrowserRouter>
+    <Modal />
+    <Header />
+    <Routes>
+        {/* <Route path="/" element={< />}>
+        <Route index element={<HomePage />} /> */}
+      {/* </Route> */}
+    {/* </Routes> */}
+  {/* </BrowserRouter> */} 
     </div>
   )
 }
